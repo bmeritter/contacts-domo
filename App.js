39f09f07d15b4da6ac8ff036a67f3cf531a56201
Qiqi Zhao <qiqzhao@thoughtworks.com>
2017-10-26 09:08:18 +0800

    add a contact component

--- App.js
+++ App.js
@@ -1,57 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, {Component} from 'react';
+import {Alert, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+export default class App extends Component {
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    onPressRow() {
+        Alert.alert("hello world");
+    }
 
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <ScrollView>
+                <TouchableOpacity onPress={() => this.onPressRow()}>
+                    <View>
+                        <Text style={styles.header}>Contacts</Text>
+                        <View style={styles.container}>
+                            <Image style={styles.picture} source={require('./app/image/001.jpeg')}/>
+                            <Text style={styles.userInfo}>username</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+
+            </ScrollView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'grey',
+    },
+    header: {
+        fontSize: 30,
+        padding: 15,
+    },
+    picture: {
+        width: 50,
+        height: 50,
+    },
+    userInfo: {
+        padding: 10,
+    },
 });
